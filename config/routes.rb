324741129812dd58f5837b1a410e6a760690a607
Matt Diebolt <mdiebolt@gmail.com>
2010-12-26 14:41:58 -0800

    removed default page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 DubsterLevels::Application.routes.draw do
+
+  get "pages/home"
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
