fcf059008446974e61aee00156187a9cbd11e0b1
Matt Diebolt <mdiebolt@gmail.com>
2010-12-26 14:18:29 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DubsterLevels::Application.config.secret_token = '82a99740b68f0b24b65ff21975ca01f7e8e706632b5aa857a1c1f07d51b43f058ba3ced0b8a48db418ae2d5ce28de208258d1eff6e4ace9d2ccaca09086c8d9c'
